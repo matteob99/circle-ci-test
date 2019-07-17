9cb3e7f376e33260bb058342d9541da29fc4d67d
matteob99 <matteo.bocci@gmail.com>
2019-07-17 16:10:34 +0200

    test circle-ci django postgres

--- CircleCiTest/settings.py
+++ CircleCiTest/settings.py
@@ -73,13 +73,25 @@ WSGI_APPLICATION = 'CircleCiTest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'CIRCLECI' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'circle_test',
+            'USER':     'root',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+            'OPTIONS': {'sslmode': 'disable'}
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
